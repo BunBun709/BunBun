525a579fbd41c3b1174b10b2e4afb2602ea10220
BúnBun709 <huydn.ecotek@gmail.com>
2023-06-02 00:18:10 +0700

    bae

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Hello bé Bành yêu của anh',
+    introDesc: `Anh có điều này muốn hỏi em phải trả lời thật lòng nha`,
+    btnIntro: 'Dạ',
+    title: 'Anh biết Em đang nhớ anh phải hong ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra là anh dỗi đó nha :> ',
+    btnYes: 'Có, em đang nhớ anh rất nhiều 💖',
+    btnNo: ' Anh mơ à !  ',
+    question:'Nói cho Anh biết đi vì sao Em nhớ anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gửi Bun! 💌💌',
+    reply: 'Tại vì anh dễ thương 😊😊',
+    mess: 'Anh biết mà, Anh cũng nhớ bae nhiều 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Dạ 💖',
+    messLink: 'https://www.facebook.com/messages/t'
+}
